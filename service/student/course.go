7f9001eae8822be97fffd5e242b664c17eabe3a5
pcccccccccc <chenjieshupdh@foxmail.com>
2022-02-11 00:20:19 +0800

    完成类型11和13的错误处理

--- service/student/course.go
+++ service/student/course.go
@@ -28,6 +28,15 @@ var paramInvalidGetStudentcourseResp = types.GetStudentCourseResponse{
 	Code: types.ParamInvalid,
 	Data: struct{ CourseList []types.TCourse }{CourseList: nil},
 }
+var studentNotExistedStudentCourseResp = types.GetStudentCourseResponse{
+	Code: types.StudentNotExisted,
+	Data: struct{ CourseList []types.TCourse }{CourseList: nil},
+}
+
+var studentHasNoCourseStudentCourseResp = types.GetStudentCourseResponse{
+	Code: types.StudentHasNoCourse,
+	Data: struct{ CourseList []types.TCourse }{CourseList: nil},
+}
 
 func GetStudentCourse(c *gin.Context) {
 	var req types.GetStudentCourseRequest
@@ -46,11 +55,20 @@ func getStudentCourseService(req *types.GetStudentCourseRequest) *types.GetStude
 	if err != nil {
 		return &paramInvalidGetStudentcourseResp
 	}
+
+	member := model.Member{}
+	//若学生ID不存在 返回错误类型11
+	result1 := database.MysqlDB.Table("members").Where("id=?", studentId).First(&member)
+	if result1.Error != nil {
+		return &studentNotExistedStudentCourseResp
+	}
 	//查找学生的课程id
-	studentCourse := model.StudentCourse{}
-	//TODO 若学生ID不存在 返回错误类型11
+	studentCourse := model.StudentCourse{CourseIdList: ""}
 	_ = database.MysqlDB.Table("book_course").Where("student_id = ?", studentId).Find(&studentCourse)
-	//TODO 若学生没有课程 返回错误类型13
+	//若学生没有课程 返回错误类型13
+	if len(studentCourse.CourseIdList) == 0 {
+		return &studentHasNoCourseStudentCourseResp
+	}
 	courseIdList := strings.Split(studentCourse.CourseIdList, ",")
 	courseList := []types.TCourse{}
 	for _, courseId := range courseIdList {
